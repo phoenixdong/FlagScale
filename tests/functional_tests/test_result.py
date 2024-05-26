4e29447cadcf385ee5a801a550119f90ad78347a
phoenixdong <liang_dong@foxmail.com>
2024-05-27 00:55:27 +0800

    update

--- tests/functional_tests/test_result.py
+++ tests/functional_tests/test_result.py
@@ -35,7 +35,7 @@ def test_result(test_reaults_path:str):
     print("The results are basically equal: ", np.allclose(gold_result_json["lm loss:"]["values"], result_json["lm loss:"]["values"]))
 
     # assert np.allclose(gold_result_json["lm loss:"]["values"], result_json["lm loss:"]["values"]), "result not close to gold result"
-    assert np.allclose(gold_result_json["lm loss:"]["values"], 11111111111111111111111), "result not close to gold result"
+    assert np.allclose(20, 11111111111111111111111), "result not close to gold result"
     
 
 if __name__ == '__main__':
